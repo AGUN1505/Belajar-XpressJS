ca94c1ba11a3a8dec6ef7ddd9bb18deaebe5f964
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-01-19 21:11:08 +0700

    chore: init prisma

--- src/services/index.service.js
+++ src/services/index.service.js
@@ -1,4 +1,7 @@
+import prisma from '../configs/database.js'
+
 export const getData = () => {
+    const admins = prisma.admin.findMany()
     const data = {
         message : 'halo',
         status: {
