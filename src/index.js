da8e83f3d797d46bf0838a6778d75906c0468eee
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-01-26 21:34:05 +0700

    feat : auth register

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import router from './routes/index.route.js'
+import authRouter from './routes/auth.route.js'
+import { errorHandler, resourceNotFound } from './middlewares/errorHandler.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.use(router)
+app.use(authRouter)
+
+app.use(resourceNotFound)
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ src/validations/auth.validator.js
@@ -0,0 +1,23 @@
+import Joi from "joi"
+
+export const registerValidator = Joi.object({
+    name: Joi.string().required().messages({
+        'string.empty': 'nama harus di isi dong',
+        'any.required' : 'isi dong'
+    }),
+    email: Joi.string().email().required().messages({
+        'string.email': 'email salah',
+        'string.empty': 'email harus di isi dong',
+        'any.required' : 'isi dong emailnya'
+    }),
+    password: Joi.string().min(6).required().messages({
+        'string.min': 'isi lebih dari 6 ya',
+        'string.empty': 'password harus di isi dong',
+        'any.required' : 'isi dong passwordnya'
+    }),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+        'string.empty': 'passwordnya harus di isi dong',
+        'any.required' : 'isi dong paswordnya',
+        'any.only' : 'passwordnya dak sama wuuu?'
+    }),
+})
